e94f9bbc6f4bea093add1803e246f29d168346a3
leticia238 <leticia.santos0808@escola.pr.gov.br>
2025-07-07 07:54:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Deodoro da Fonseca foi o primeiro presidente do Brasil'
+)
+
+criaCartao(
+    'Ciência',
+    'Qual é o estado da água a 100 graus Celsius?',
+    'A água está no estado gasoso a 100 graus Celsius'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 7 vezes 8?',
+    '7 vezes 8 é 56'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual país sediou a Copa do Mundo de 2022?',
+    'O Catar sediou a Copa do Mundo de 2022'
+)
